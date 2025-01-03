f8452a1758af0a9187d75790836a41ac8cb62641
Muhammad Alfian <alfiandwi48@gmail.com>
2025-01-03 14:54:56 +0700

    feat: :sparkles: add authentication and validation
    :safety_vest: add validation form
    
     :passport_control: add auth control

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,10 +1,9 @@
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
-import { fetchCardData } from "../../lib/data";
 
 export default async function Page() {
 
